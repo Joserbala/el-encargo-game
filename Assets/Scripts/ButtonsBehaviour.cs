6100284f818fe1ab7be566beb8b85af5e42f94d6
José Gala Naranjo <jgalanar@gmail.com>
2021-01-29 02:29:20 +0100

    Fix dragging of the player, 1st approach to menu
    Fix dragging of the player: it was dragged when the UI was present but
    it won't more thanks to IsPointerOverGameObject.
    First approach to main menu.

---
+++ Assets/Scripts/ButtonsBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsPanel;
+    private bool isOptionsPanelActive = false;
+
+    public void SetActiveOptionsMenu()
+    {
+        isOptionsPanelActive = !isOptionsPanelActive;
+        optionsPanel.SetActive(isOptionsPanelActive);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
