973b8654d8f43a4d58d492ddae57a102b149d183
José Gala Naranjo <jgalanar@gmail.com>
2021-02-10 02:09:50 +0100

    First approach to GameManager: Pause

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameIsPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseResumeGame();
+        }
+    }
+
+    private void PauseResumeGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+        }
+    }
+}
